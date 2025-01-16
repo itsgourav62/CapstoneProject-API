78622a5f268f96ba4b34c0861cbffc676a0cd2b5
Gourav Shaw <shawgourav62@gmail.com>
2025-01-16 10:18:26 +0530

    Pulling from main Branch (#15)
    * Revert "Adding Previous Code"
    
    * Remove unused files and interfaces, and add ERole and EPaymentStatus enums
    
    * Remove unused qwikpay files and consolidate .gitignore and README.md
    
    * Remove ERole enum as it is no longer needed
    
    * Added Code for Exception Handling for USer
    
    * commited the code (#12)
    
    Co-authored-by: Harshitha-cpu <harshitha.b@slkgroup.com>
    
    * Removed the comment (#13)
    
    * commited the code
    
    * removed the code
    
    ---------
    
    Co-authored-by: Harshitha-cpu <harshitha.b@slkgroup.com>
    
    ---------
    
    Co-authored-by: Harshitha-cpu <harshitha.b@slkgroup.com>
    Co-authored-by: Harshitha-cpu <harshi583121@gmail.com>

--- server/qwikpay/src/main/java/com/capstone/qwikpay/services/UserServiceImpl.java
+++ server/qwikpay/src/main/java/com/capstone/qwikpay/services/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.capstone.qwikpay.entities.UserEntity;
+import com.capstone.qwikpay.exceptions.UserNotFoundException;
 import com.capstone.qwikpay.repositories.UserRepository;
 
 @Service
@@ -15,38 +16,46 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserRepository repo;
 	
-	
+	//Add new user
 	@Override
 	public UserEntity addNewUser(UserEntity user) {
-		// TODO Auto-generated method stub
 		return repo.save(user);
 	}
-	
+	//Get user by ID with exception handling
 	@Override
-	public UserEntity getUser(Integer userId) {
-		// TODO Auto-generated method stub
+	public UserEntity getUser(Integer userId) throws UserNotFoundException {
 		Optional<UserEntity> user = repo.findById(userId);
 		if(user.isPresent()) {
 			return user.get();
+		}else {
+			//throw exception if user not found
+			throw new UserNotFoundException("User with ID " + userId + " not found");
 		}
-		return null;
+	
 	}
 
 	@Override
-	public UserEntity updateUser(UserEntity user) {
-		// TODO Auto-generated method stub
-		if(repo.existsById(user.getId())) {
-			UserEntity updateUser= repo.save(user);
-		}
-		return user;
-	}
+    public UserEntity updateUser(UserEntity user) throws UserNotFoundException {
+        // Check if the user exists
+        if (repo.existsById(user.getId())) {
+            // Save and update user
+            return repo.save(user);
+        } else {
+            // Throw exception if user does not exist
+            throw new UserNotFoundException("User with ID " + user.getId() + " not found");
+        }
+    }
 
 	@Override
-	public String deleteUser(Integer userId) {
-		// TODO Auto-generated method stub
-		repo.deleteById(userId);
-		return "User with customer id "+userId+" deleted successfully";
-	}
+    public String deleteUser(Integer userId) throws UserNotFoundException {
+        if (repo.existsById(userId)) {
+            repo.deleteById(userId);
+            return "User with ID " + userId + " deleted successfully";
+        } else {
+            // Throw exception if user to delete doesn't exist
+            throw new UserNotFoundException("User with ID " + userId + " not found");
+        }
+    }
 
 	@Override
 	public List<UserEntity> getUsers() {
