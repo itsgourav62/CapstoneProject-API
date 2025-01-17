2f597e30ec1f2a234b9ba8aa01f5505a6191f8c8
itsgourav62 <shawgourav62@gmail.com>
2025-01-17 16:48:10 +0530

    refactor routing and improve home component layout

--- client/QwikPayApp/src/app/app-routing.module.ts
+++ client/QwikPayApp/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './users/home/home.component';
-import { HomeComponent } from './users/home/home.component';
+
 import { SigninComponent } from './users/signin/signin.component';
 import { SignupComponent } from './users/signup/signup.component';
 import { ForgotPasswordComponent } from './users/forgotpassword/forgotpassword.component';
@@ -14,18 +14,6 @@ const routes: Routes = [
 
 ];
 
-import { HomeComponent } from './users/home/home.component';
-import { SigninComponent } from './users/signin/signin.component';
-import { SignupComponent } from './users/signup/signup.component';
-import { ForgotPasswordComponent } from './users/forgotpassword/forgotpassword.component';
-
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'signin', component: SigninComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'forgotpassword', component: ForgotPasswordComponent }
-
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
