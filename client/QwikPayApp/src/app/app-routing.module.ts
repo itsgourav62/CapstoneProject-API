8cd2ccda64e42c8da0a1817fdb4e12f4419df2b1
itsgourav62 <shawgourav62@gmail.com>
2025-01-17 15:59:21 +0530

    refactor routing and module imports for cleaner structure

--- client/QwikPayApp/src/app/app-routing.module.ts
+++ client/QwikPayApp/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { HomeComponent } from './users/home/home.component';
 import { SigninComponent } from './users/signin/signin.component';
 import { SignupComponent } from './users/signup/signup.component';
@@ -13,10 +12,6 @@ const routes: Routes = [
   { path: 'forgotpassword', component: ForgotPasswordComponent }
 
 ];
-
-
-const routes: Routes = [];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/QwikPayApp/src/app/app.module.ts
+++ client/QwikPayApp/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
 //import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -15,17 +14,6 @@ import { ForgotPasswordComponent } from './users/forgotpassword/forgotpassword.c
 
 // PrimeNG Modules=
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './users/home/home.component';
-import { SigninComponent } from './users/signin/signin.component';
-import { SignupComponent } from './users/signup/signup.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,16 +21,14 @@ import { HeaderComponent } from './shared/header/header.component';
     SigninComponent,
     SignupComponent,
     FooterComponent,
-    ForgotPasswordComponent,
-    HeaderComponent
-
+    HeaderComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule
-    
 
   ],
   providers: [],
