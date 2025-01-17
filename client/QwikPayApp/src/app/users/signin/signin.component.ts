039936b3ecb8758dc920b57ef4525440e274edfb
itsgourav62 <shawgourav62@gmail.com>
2025-01-17 14:32:53 +0530

    added signin and signup and forgot password feature with form validation and routing

--- client/QwikPayApp/src/app/users/signin/signin.component.ts
+++ client/QwikPayApp/src/app/users/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
+  signinForm: FormGroup;
+  errorMessage: string = '';
 
+  constructor(private fb: FormBuilder) {
+    this.signinForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.signinForm.valid) {
+      console.log('Form Submitted', this.signinForm.value);
+    } else {
+      this.errorMessage = 'Please fill out the form correctly.';
+    }
+  }
 }
