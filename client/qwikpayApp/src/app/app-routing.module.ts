ebd0fc02217dddb82227c61b8a768ac852f88ea7
KotlaChenna <2000040320@kluniversity.in>
2025-01-17 10:32:53 +0530

    added header and footer and home components

--- client/qwikpayApp/src/app/app-routing.module.ts
+++ client/qwikpayApp/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to Home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
