e33a572aa6860a876b22893a8f4b82b2e49abf1b
Gourav Shaw <shawgourav62@gmail.com>
2025-01-17 14:57:35 +0530

    added signin and signup and forgot password feature with form validation and routing (#32)
    * Pulling from main Branch (#15)
    
    * Revert "Adding Previous Code"
    
    * Remove unused files and interfaces, and add ERole and EPaymentStatus enums
    
    * Remove unused qwikpay files and consolidate .gitignore and README.md
    
    * Remove ERole enum as it is no longer needed
    
    * Added Code for Exception Handling for USer
    
    * commited the code (#12)
    
    Co-authored-by: Harshitha-cpu <harshitha.b@slkgroup.com>
    
    * Removed the comment (#13)
    
    * commited the code
    
    * removed the code
    
    ---------
    
    Co-authored-by: Harshitha-cpu <harshitha.b@slkgroup.com>
    
    ---------
    
    Co-authored-by: Harshitha-cpu <harshitha.b@slkgroup.com>
    Co-authored-by: Harshitha-cpu <harshi583121@gmail.com>
    
    * Remove unnecessary attributes from .classpath and disable unused plugin in .factorypath
    
    * Remove Transaction-related classes and clean up application configuration
    
    * Add user sign-in and sign-up components with initial setup
    
    * removed
    
    * add .gitignore file to exclude dependencies, build output, environment files, logs, IDE files, and OS-specific files
    
    * replace .gitignore file with a comprehensive version to exclude additional dependencies, build outputs, environment files, logs, IDE files, and OS-specific files
    
    * add TypeScript configuration files for application and testing
    
    * remove unused components, styles, and configuration files
    
    * initialize QwikPayApp with basic structure and components
    
    * added signin and signup and forgot password feature with form validation and routing
    
    ---------
    
    Co-authored-by: Harshitha-cpu <harshitha.b@slkgroup.com>
    Co-authored-by: Harshitha-cpu <harshi583121@gmail.com>
    Co-authored-by: Rohan Rathod <97231075+Rockingrohan@users.noreply.github.com>

--- client/QwikPayApp/src/app/app.module.ts
+++ client/QwikPayApp/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './users/home/home.component';
+import { SigninComponent } from './users/signin/signin.component';
+import { SignupComponent } from './users/signup/signup.component';
+import { ForgotPasswordComponent } from './users/forgotpassword/forgotpassword.component';
+
+// PrimeNG Modules=
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
-// PrimeNG Modules
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { DropdownModule } from 'primeng/dropdown';
 import { HomeComponent } from './users/home/home.component';
 import { SigninComponent } from './users/signin/signin.component';
 import { SignupComponent } from './users/signup/signup.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+
 
 @NgModule({
   declarations: [
@@ -23,16 +33,16 @@ import { HeaderComponent } from './shared/header/header.component';
     SigninComponent,
     SignupComponent,
     FooterComponent,
+    ForgotPasswordComponent,
     HeaderComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ButtonModule,
-    TableModule,
-    DropdownModule
+    ReactiveFormsModule,
+    FormsModule
+    
 
   ],
   providers: [],
